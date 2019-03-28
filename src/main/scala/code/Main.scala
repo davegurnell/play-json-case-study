c86b5907ca059c3a31463e088a479f5a1a6851ba
Dave Gurnell <dave@untyped.com>
2019-03-28 08:37:50 +0000

    Exercises

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -12,8 +12,6 @@ case class MessageId(value: Long = -1L)
 object MessageId {
   // Define a Format[Message]
   // that serializes a message to a string
-  implicit val format: Format[MessageId] =
-    implicitly[Format[Long]].inmap(MessageId(_), _.value)
 }
 
 // ----------------------------------------------
@@ -30,8 +28,6 @@ case class Message(
 object Message {
   // Define an OFormat[Message]
   // that serializes a message to an object
-  implicit val format: OFormat[Message] =
-    Json.format
 }
 
 // ----------------------------------------------
@@ -44,31 +40,6 @@ final case object Green extends TrafficLight
 object TrafficLight {
   // Define a Format[TrafficLight]
   // that serializes a value to an integer
-  implicit val format: Format[TrafficLight] =
-    new Format[TrafficLight] {
-      override def reads(in: JsValue): JsResult[TrafficLight] =
-        in match {
-          case JsNumberAsInt(0) => JsSuccess(Red)
-          case JsNumberAsInt(1) => JsSuccess(Amber)
-          case JsNumberAsInt(2) => JsSuccess(Green)
-          case _                => JsError("Badness!")
-        }
-
-      override def writes(in: TrafficLight): JsValue =
-        in match {
-          case Red   => JsNumber(0)
-          case Amber => JsNumber(1)
-          case Green => JsNumber(2)
-        }
-    }
-
-  private object JsNumberAsInt {
-    def unapply(value: JsValue): Option[Int] =
-      value match {
-        case JsNumber(num) => Some(num.toInt)
-        case _             => None
-      }
-  }
 }
 
 // ----------------------------------------------
@@ -79,17 +50,6 @@ object ColorFormats {
   // Define an OFormat[Color]
   // that serializes a Color to an object
   // with fields for red, green, and blue
-  implicit val format: OFormat[Color] = (
-    (__ \ "red").format[Int] ~
-    (__ \ "green").format[Int] ~
-    (__ \ "blue").format[Int]
-  )(create, extract)
-
-  def create(r: Int, g: Int, b: Int): Color =
-    new Color(r, g, b)
-
-  def extract(c: Color): (Int, Int, Int) =
-    (c.getRed, c.getGreen, c.getBlue)
 }
 
 // ----------------------------------------------
@@ -104,25 +64,6 @@ object Animal {
   // that serializes an animal to an object
   // with a "type" discriminator field
   // and other fields as appropriate
-  val dogFormat: OFormat[Dog] = Json.format
-  val insectFormat: OFormat[Insect] = Json.format
-  val swallowFormat: OFormat[Swallow] = Json.format
-
-  implicit val format: Format[Animal] =
-    new Format[Animal] {
-      def reads(in: JsValue) = (in \ "type") match {
-        case JsDefined(JsString("Dog"))     => dogFormat.reads(in)
-        case JsDefined(JsString("Insect"))  => insectFormat.reads(in)
-        case JsDefined(JsString("Swallow")) => swallowFormat.reads(in)
-        case _                              => JsError("Badness")
-      }
-
-      def writes(in: Animal) = in match {
-        case in: Dog     => dogFormat.writes(in)     ++ Json.obj("type" -> "Dog")
-        case in: Insect  => insectFormat.writes(in)  ++ Json.obj("type" -> "Insect")
-        case in: Swallow => swallowFormat.writes(in) ++ Json.obj("type" -> "Swallow")
-      }
-    }
 }
 
 // ----------------------------------------------
@@ -135,27 +76,26 @@ object Main extends App {
   }
 
   println("MessageId")
-  roundTrip(MessageId())
-  println(JsNull.validate[MessageId])
+  // roundTrip(MessageId())
+  // println(JsNull.validate[MessageId])
 
   println("Message")
-  roundTrip(Message("Author", "Text"))
-  println(JsNull.validate[Message])
+  // roundTrip(Message("Author", "Text"))
+  // println(JsNull.validate[Message])
 
   println("Color")
-  import ColorFormats._
-  roundTrip(new Color(0, 127, 255))
-  println(JsNull.validate[Color])
+  // roundTrip(new Color(0, 127, 255))
+  // println(JsNull.validate[Color])
 
   println("TrafficLight")
-  roundTrip(Red : TrafficLight)
-  roundTrip(Amber : TrafficLight)
-  roundTrip(Green : TrafficLight)
-  println(JsNull.validate[TrafficLight])
+  // roundTrip(Red)
+  // roundTrip(Amber)
+  // roundTrip(Green)
+  // println(JsNull.validate[TrafficLight])
 
   println("Animal")
-  roundTrip(Dog("Sparky") : Animal)
-  roundTrip(Insect(6) : Animal)
-  roundTrip(Swallow(100) : Animal)
-  println(JsNull.validate[Animal])
+  // roundTrip(Dog("Sparky"))
+  // roundTrip(Insect(6))
+  // roundTrip(Swallow(100))
+  // println(JsNull.validate[Animal])
 }
